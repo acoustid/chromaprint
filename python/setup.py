f65eb7ef8cba04a2b9b25d0fa4a0f39faff46ce7
Lukáš Lalinský <lalinsky@gmail.com>
2011-04-07 16:44:49 +0200

    Simple Python bindings based on ctypes

---
+++ python/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Chromaprint',
+      version='0.3',
+      description='Python bindings for Chromaprint',
+      author='Lukas Lalinsky',
+      author_email='lalinsky@gmail.com',
+      url='http://wiki.acoustid.org/wiki/Chromaprint',
+      packages=['chromaprint'],
+     )
+
