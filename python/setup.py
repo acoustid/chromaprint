7b1b6e232941d8005ab7f89faf5eee9421fe24c1
Lukáš Lalinský <lalinsky@gmail.com>
2011-10-06 11:33:18 +0200

    Increment version

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Chromaprint',
-      version='0.4',
+      version='0.5',
       description='Python bindings for Chromaprint',
       author='Lukas Lalinsky',
       author_email='lalinsky@gmail.com',
