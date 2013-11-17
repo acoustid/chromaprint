3f463606db90d45470818d0838f3f902ebb91129
Gordon Pettey <petteyg@foldspace.org>
2013-11-17 18:26:30 +0000

    Cleanup for pull request #2

--- examples/fpcalc.c
+++ examples/fpcalc.c
@@ -30,9 +30,9 @@ int decode_audio_file(ChromaprintContext *chromaprint_ctx, const char *file_name
 	AVStream *stream = NULL;
 	AVFrame *frame = NULL;
 #if defined(HAVE_SWRESAMPLE)
-	SwrContext *swr_ctx = NULL;
+	SwrContext *convert_ctx = NULL;
 #elif defined(HAVE_AVRESAMPLE)
-	AVAudioResampleContext *avr_ctx = NULL;
+	AVAudioResampleContext *convert_ctx = NULL;
 #endif
 	int max_dst_nb_samples = 0, dst_linsize = 0;
 	uint8_t *dst_data[1] = { NULL };
@@ -76,40 +76,36 @@ int decode_audio_file(ChromaprintContext *chromaprint_ctx, const char *file_name
 	}
 
 	if (codec_ctx->sample_fmt != AV_SAMPLE_FMT_S16) {
-#if !defined(HAVE_SWRESAMPLE) && !defined(HAVE_AVRESAMPLE)
-		fprintf(stderr, "ERROR: unsupported audio format (please build fpcalc with libswresample)\n");
-		goto done;
-#endif
 		int64_t channel_layout = codec_ctx->channel_layout;
 		if (!channel_layout) {
 			channel_layout = av_get_default_channel_layout(codec_ctx->channels);
 		}
 #if defined(HAVE_SWRESAMPLE)
-		swr_ctx = swr_alloc_set_opts(NULL,
+		convert_ctx = swr_alloc_set_opts(NULL,
 			channel_layout, AV_SAMPLE_FMT_S16, codec_ctx->sample_rate,
 			channel_layout, codec_ctx->sample_fmt, codec_ctx->sample_rate,
 			0, NULL);
-		if (!swr_ctx) {
+		if (!convert_ctx) {
 			fprintf(stderr, "ERROR: couldn't allocate audio converter\n");
 			goto done;
 		}
-		if (swr_init(swr_ctx) < 0) {
+		if (swr_init(convert_ctx) < 0) {
 			fprintf(stderr, "ERROR: couldn't initialize the audio converter\n");
 			goto done;
 		}
 #elif defined(HAVE_AVRESAMPLE)
-		avr_ctx = avresample_alloc_context();
-		av_opt_set_int(avr_ctx, "out_channel_layout", channel_layout, 0);
-		av_opt_set_int(avr_ctx, "out_sample_fmt", AV_SAMPLE_FMT_S16, 0);
-		av_opt_set_int(avr_ctx, "out_sample_rate", codec_ctx->sample_rate, 0);
-		av_opt_set_int(avr_ctx, "in_channel_layout", channel_layout, 0);
-		av_opt_set_int(avr_ctx, "in_sample_fmt", codec_ctx->sample_fmt, 0);
-		av_opt_set_int(avr_ctx, "in_sample_rate", codec_ctx->sample_rate, 0);
-		if (!avr_ctx) {
+		convert_ctx = avresample_alloc_context();
+		av_opt_set_int(convert_ctx, "out_channel_layout", channel_layout, 0);
+		av_opt_set_int(convert_ctx, "out_sample_fmt", AV_SAMPLE_FMT_S16, 0);
+		av_opt_set_int(convert_ctx, "out_sample_rate", codec_ctx->sample_rate, 0);
+		av_opt_set_int(convert_ctx, "in_channel_layout", channel_layout, 0);
+		av_opt_set_int(convert_ctx, "in_sample_fmt", codec_ctx->sample_fmt, 0);
+		av_opt_set_int(convert_ctx, "in_sample_rate", codec_ctx->sample_rate, 0);
+		if (!convert_ctx) {
 			fprintf(stderr, "ERROR: couldn't allocate audio converter\n");
 			goto done;
 		}
-		if (avresample_open(avr_ctx) < 0) {
+		if (avresample_open(convert_ctx) < 0) {
 			fprintf(stderr, "ERROR: couldn't initialize the audio converter\n");
 			goto done;
 		}
@@ -149,11 +145,7 @@ int decode_audio_file(ChromaprintContext *chromaprint_ctx, const char *file_name
 
 			if (got_frame) {
 				data = frame->data;
-#if defined(HAVE_SWRESAMPLE)
-				if (swr_ctx) {
-#elif defined(HAVE_AVRESAMPLE)
-				if (avr_ctx) {
-#endif
+				if (convert_ctx) {
 					if (frame->nb_samples > max_dst_nb_samples) {
 						av_freep(&dst_data[0]);
 						if (av_samples_alloc(dst_data, &dst_linsize, codec_ctx->channels, frame->nb_samples, AV_SAMPLE_FMT_S16, 1) < 0) {
@@ -163,9 +155,9 @@ int decode_audio_file(ChromaprintContext *chromaprint_ctx, const char *file_name
 						max_dst_nb_samples = frame->nb_samples;
 					}
 #if defined(HAVE_SWRESAMPLE)
-					if (swr_convert(swr_ctx, dst_data, frame->nb_samples, (const uint8_t **)frame->data, frame->nb_samples) < 0) {
+					if (swr_convert(convert_ctx, dst_data, frame->nb_samples, (const uint8_t **)frame->data, frame->nb_samples) < 0) {
 #elif defined(HAVE_AVRESAMPLE)
-					if (avresample_convert(avr_ctx, dst_data, 0, frame->nb_samples, (const uint8_t **)frame->data, 0, frame->nb_samples) < 0) {
+					if (avresample_convert(convert_ctx, dst_data, 0, frame->nb_samples, (const uint8_t **)frame->data, 0, frame->nb_samples) < 0) {
 #endif
 						fprintf(stderr, "ERROR: couldn't convert the audio\n");
 						goto done;
@@ -200,21 +192,16 @@ done:
 	if (frame) {
 		avcodec_free_frame(&frame);
 	}
-#if defined(HAVE_SWRESAMPLE)
 	if (dst_data[0]) {
 		av_freep(&dst_data[0]);
 	}
-	if (swr_ctx) {
-		swr_free(&swr_ctx);
-	}
+	if (convert_ctx) {
+#if defined(HAVE_SWRESAMPLE)
+		swr_free(&convert_ctx);
 #elif defined(HAVE_AVRESAMPLE)
-	if(dst_data[0]) {
-		av_freep(&dst_data[0]);
-	}
-	if (avr_ctx) {
-		avresample_free(&avr_ctx);
-	}
+		avresample_free(&convert_ctx);
 #endif
+	}
 	if (codec_ctx_opened) {
 		avcodec_close(codec_ctx);
 	}
