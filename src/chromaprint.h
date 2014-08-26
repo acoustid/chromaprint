2065d46d76ff3d3db63d4c15de5a79c614a0be20
Lukáš Lalinský <lukas@oxygene.sk>
2014-08-26 18:57:36 +0200

    Version 1.2

--- src/chromaprint.h
+++ src/chromaprint.h
@@ -46,7 +46,7 @@ extern "C" {
 typedef void *ChromaprintContext;
 
 #define CHROMAPRINT_VERSION_MAJOR 1
-#define CHROMAPRINT_VERSION_MINOR 1
+#define CHROMAPRINT_VERSION_MINOR 2
 #define CHROMAPRINT_VERSION_PATCH 0
 
 enum ChromaprintAlgorithm {
