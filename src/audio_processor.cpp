f24801cc4246d9cfd2ec07ec5229af5460296b93
Lukáš Lalinský <lukas@oxygene.sk>
2016-05-29 15:47:52 +0200

    Use std::vector in AudioProcessor

--- src/audio_processor.cpp
+++ src/audio_processor.cpp
@@ -1,22 +1,5 @@
-/*
- * Chromaprint -- Audio fingerprinting toolkit
- * Copyright (C) 2010-2011  Lukas Lalinsky <lalinsky@gmail.com>
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- * USA
- */
+// Copyright (C) 2010-2016  Lukas Lalinsky
+// Distributed under the MIT license, see the LICENSE file for details.
 
 #include <assert.h>
 #include <algorithm>
@@ -27,7 +10,7 @@ extern "C" {
 #include "debug.h"
 #include "audio_processor.h"
 
-using namespace chromaprint;
+namespace chromaprint {
 
 static const int kMinSampleRate = 1000;
 static const int kMaxBufferSize = 1024 * 16;
@@ -39,14 +22,13 @@ static const int kResampleLinear = 0;
 static const double kResampleCutoff = 0.8;
 
 AudioProcessor::AudioProcessor(int sample_rate, AudioConsumer *consumer)
-	: m_buffer_size(kMaxBufferSize),
+	: m_buffer(kMaxBufferSize),
+	  m_buffer_offset(0),
+	  m_resample_buffer(kMaxBufferSize),
 	  m_target_sample_rate(sample_rate),
 	  m_consumer(consumer),
 	  m_resample_ctx(0)
 {
-	m_buffer = new short[kMaxBufferSize];
-	m_buffer_offset = 0;
-	m_resample_buffer = new short[kMaxBufferSize];
 }
 
 AudioProcessor::~AudioProcessor()
@@ -54,13 +36,11 @@ AudioProcessor::~AudioProcessor()
 	if (m_resample_ctx) {
 		av_resample_close(m_resample_ctx);
 	}
-	delete[] m_resample_buffer;
-	delete[] m_buffer;
 }
 
 void AudioProcessor::LoadMono(short *input, int length)
 {
-	short *output = m_buffer + m_buffer_offset;
+	short *output = m_buffer.data() + m_buffer_offset;
 	while (length--) {
 		*output++ = input[0];
 		input++;
@@ -69,7 +49,7 @@ void AudioProcessor::LoadMono(short *input, int length)
 
 void AudioProcessor::LoadStereo(short *input, int length)
 {
-	short *output = m_buffer + m_buffer_offset;
+	short *output = m_buffer.data() + m_buffer_offset;
 	while (length--) {
 		*output++ = (input[0] + input[1]) / 2;
 		input += 2;
@@ -78,7 +58,7 @@ void AudioProcessor::LoadStereo(short *input, int length)
 
 void AudioProcessor::LoadMultiChannel(short *input, int length)
 {
-	short *output = m_buffer + m_buffer_offset;
+	short *output = m_buffer.data() + m_buffer_offset;
 	while (length--) {
 		long sum = 0;
 		for (int i = 0; i < m_num_channels; i++) {
@@ -91,8 +71,8 @@ void AudioProcessor::LoadMultiChannel(short *input, int length)
 int AudioProcessor::Load(short *input, int length)
 {
 	assert(length >= 0);
-	assert(m_buffer_offset <= m_buffer_size);
-	length = std::min(length, m_buffer_size - m_buffer_offset);
+	assert(m_buffer_offset <= m_buffer.size());
+	length = std::min(length, static_cast<int>(m_buffer.size() - m_buffer_offset));
 	switch (m_num_channels) {
 	case 1:
 		LoadMono(input, length);
@@ -111,20 +91,20 @@ int AudioProcessor::Load(short *input, int length)
 void AudioProcessor::Resample()
 {
 	if (!m_resample_ctx) {
-		m_consumer->Consume(m_buffer, m_buffer_offset);
+		m_consumer->Consume(m_buffer.data(), m_buffer_offset);
 		m_buffer_offset = 0;
 		return;
 	}
 	int consumed = 0;
-	int length = av_resample(m_resample_ctx, m_resample_buffer, m_buffer, &consumed, m_buffer_offset, kMaxBufferSize, 1);
+	int length = av_resample(m_resample_ctx, m_resample_buffer.data(), m_buffer.data(), &consumed, m_buffer_offset, kMaxBufferSize, 1);
 	if (length > kMaxBufferSize) {
 		DEBUG("chromaprint::AudioProcessor::Resample() -- Resampling overwrote output buffer.");
 		length = kMaxBufferSize;
 	}
-	m_consumer->Consume(m_resample_buffer, length);
+	m_consumer->Consume(m_resample_buffer.data(), length);
 	int remaining = m_buffer_offset - consumed;
 	if (remaining > 0) {
-		std::copy(m_buffer + consumed, m_buffer + m_buffer_offset, m_buffer);
+		std::copy(m_buffer.begin() + consumed, m_buffer.begin() + m_buffer_offset, m_buffer.begin());
 	}
 	else if (remaining < 0) {
 		DEBUG("chromaprint::AudioProcessor::Resample() -- Resampling overread input buffer.");
@@ -169,11 +149,11 @@ void AudioProcessor::Consume(short *input, int length)
 	length /= m_num_channels;
 	while (length > 0) {
 		int consumed = Load(input, length); 
-		input += consumed * m_num_channels;		
+		input += consumed * m_num_channels;
 		length -= consumed;
-		if (m_buffer_size == m_buffer_offset) {
+		if (m_buffer.size() == m_buffer_offset) {
 			Resample();
-			if (m_buffer_size == m_buffer_offset) {
+			if (m_buffer.size() == m_buffer_offset) {
 				DEBUG("chromaprint::AudioProcessor::Consume() -- Resampling failed?");
 				return;
 			}
@@ -188,3 +168,4 @@ void AudioProcessor::Flush()
 	}
 }
 
+}; // namespace chromaprint
--- src/audio_processor.h
+++ src/audio_processor.h
@@ -1,22 +1,5 @@
-/*
- * Chromaprint -- Audio fingerprinting toolkit
- * Copyright (C) 2010-2011  Lukas Lalinsky <lalinsky@gmail.com>
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
- * USA
- */
+// Copyright (C) 2010-2016  Lukas Lalinsky
+// Distributed under the MIT license, see the LICENSE file for details.
 
 #ifndef CHROMAPRINT_AUDIO_PROCESSOR_H_
 #define CHROMAPRINT_AUDIO_PROCESSOR_H_
@@ -71,10 +54,9 @@ namespace chromaprint
 		void LoadMultiChannel(short *input, int length);
 		void Resample();
 
-		short *m_buffer;
-		short *m_resample_buffer;
-		int m_buffer_offset;
-		int m_buffer_size;
+		std::vector<short> m_buffer;
+		size_t m_buffer_offset;
+		std::vector<short> m_resample_buffer;
 		int m_target_sample_rate;
 		int m_num_channels;
 		AudioConsumer *m_consumer;
