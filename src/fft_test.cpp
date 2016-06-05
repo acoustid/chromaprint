3d50bbe0340a91a5bc86be38c51df0d3605f1b8f
Lukáš Lalinský <lukas@oxygene.sk>
2016-06-05 22:44:46 +0200

    Less strict check

--- src/fft_test.cpp
+++ src/fft_test.cpp
@@ -71,7 +71,7 @@ TEST(FFTTest, Sine) {
 		std::stringstream ss;
 		for (size_t i = 0; i < frame.size(); i++) {
 			const auto magnitude = sqrt(frame[i]) / frame.size();
-			EXPECT_NEAR(expected_spectrum[i], magnitude, 0.0001) << "magnitude different at offset " << i;
+			EXPECT_NEAR(expected_spectrum[i], magnitude, 0.001) << "magnitude different at offset " << i;
 			ss << "s[" << i << "]=" << magnitude << " ";
 		}
 //		DEBUG("spectrum " << ss.str());
