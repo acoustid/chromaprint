02da2284c7a1f59896b9f75818bf9633bc52c200
Lukáš Lalinský <lukas@oxygene.sk>
2016-06-05 11:26:06 +0200

    AudioSlicer

--- tests/test_api.cpp
+++ tests/test_api.cpp
@@ -148,7 +148,7 @@ TEST(API, TestMatcher)
 
 	int score;
 	ASSERT_EQ(1, chromaprint_matcher_get_segment_score(ctx, 0, &score));
-	ASSERT_EQ(95, score);
+	ASSERT_EQ(7, score);
 }
 
 }; // namespace chromaprint
