1c649c308677394e183f2635c8b998d999b14166
Lukáš Lalinský <lukas@oxygene.sk>
2016-11-23 20:48:33 +0100

    Stop using std::round

--- src/fingerprint_matcher.h
+++ src/fingerprint_matcher.h
@@ -31,7 +31,7 @@ struct Segment
 	int public_score() const {
 		//return std::max(0, int(100 - std::round(score * (100.0 / 14.0))));
 		//return std::max(0, int(100 - std::round(score * (100.0 / 14.0))));
-		return std::round(score * 100);
+		return int(score * 100 + 0.5);
 	}
 
 	Segment merged(const Segment &other) {
