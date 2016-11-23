e53f24f4a215d1c1eece531be4a4569c21351da5
Lukáš Lalinský <lukas@oxygene.sk>
2016-11-23 20:02:21 +0100

    MacOS builds

--- package/build.sh
+++ package/build.sh
@@ -12,7 +12,7 @@ trap 'rm -rf $TMP_BUILD_DIR' EXIT
 
 cd $TMP_BUILD_DIR
 
-wget -q -O artifacts.zip "https://code.oxygene.sk/acoustid/ffmpeg-build/builds/artifacts/master/download?job=build%3A$OS-$ARCH"
+wget -q -O artifacts.zip "https://code.oxygene.sk/acoustid/ffmpeg-build/builds/artifacts/master/download?job=$OS+$ARCH"
 unzip artifacts.zip
 export FFMPEG_DIR=$TMP_BUILD_DIR/$(ls -d ffmpeg-* | tail)
 
@@ -26,6 +26,10 @@ CMAKE_ARGS=(
 
 STRIP=strip
 
+OSX_CC=/usr/local/bin/gcc-5
+OSX_SDK=/Developer/SDKs/MacOSX10.4u.sdk
+OSX_VERSION=10.4
+
 case $OS in
 windows)
     perl -pe "s!{EXTRA_PATHS}!$FFMPEG_DIR!g" $BASE_DIR/package/toolchain-mingw.cmake.in | perl -pe "s!{ARCH}!$ARCH!g" >toolchain.cmake
@@ -36,11 +40,23 @@ windows)
     )
     STRIP=$ARCH-w64-mingw32-strip
     ;;
+macos)
+    CMAKE_ARGS+=(
+        -DCMAKE_TOOLCHAIN_FILE=$TMP_BUILD_DIR/toolchain.cmake
+        -DCMAKE_C_COMPILER=$OSX_CC
+        -DCMAKE_CXX_COMPILER=$OSX_CXX
+        -DCMAKE_OSX_SYSROOT=$OSX_SDK
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=$OSX_VERSION
+        -DCMAKE_OSX_ARCHITECTURES=$ARCH
+    )
+    ;;
 linux)
     case $ARCH in
     i686)
-        export CC='gcc -m32'
-        export CXX='g++ -m32'
+        CMAKE_ARGS+=(
+            -DCMAKE_C_FLAGS='-m32'
+            -DCMAKE_CXX_FLAGS='-m32'
+        )
         ;;
     x86_64)
         ;;
