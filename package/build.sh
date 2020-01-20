f24b7d6731dec0511c915f2570805e02bd77b081
Philipp Wolfer <ph.wolfer@gmail.com>
2020-01-20 07:53:20 +0100

    Use new ffmpeg 4.2.2 builds

--- package/build.sh
+++ package/build.sh
@@ -14,8 +14,7 @@ trap 'rm -rf $TMP_BUILD_DIR' EXIT
 
 cd $TMP_BUILD_DIR
 
-curl -s -L "https://code.oxygene.sk/acoustid/ffmpeg-build/builds/artifacts/master/download?job=$OS+$ARCH" > artifacts.zip
-unzip artifacts.zip
+curl -s -L "https://github.com/acoustid/ffmpeg-build/releases/download/v4.2.2-2/ffmpeg-4.2.2-audio-$OS-$ARCH.tar.gz" | tar xz
 export FFMPEG_DIR=$TMP_BUILD_DIR/$(ls -d ffmpeg-* | tail)
 
 CMAKE_ARGS=(
