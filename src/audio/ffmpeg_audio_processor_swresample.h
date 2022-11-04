8ccad6937177b1b92e40ab8f4447ea27bac009a7
Lukáš Lalinský <lalinsky@gmail.com>
2022-11-04 21:47:38 +0100

    Use FFmpeg 5.x (#120)
    * Use FFmpeg 5.1.2 for CI builds
    
    * Build on Ubuntu 20.04
    
    * Upgrade code to FFmpeg 5.x APIs
    
    * Only set FFmpeg include dirs if building tools
    
    * No longer needed
    
    * Use ubuntu 20.04

--- src/audio/ffmpeg_audio_processor_swresample.h
+++ src/audio/ffmpeg_audio_processor_swresample.h
@@ -28,30 +28,28 @@ public:
 		av_opt_set_double(m_swr_ctx, "cutoff", 0.8, 0);
 	}
 
-	void SetInputChannelLayout(int64_t channel_layout) {
-		av_opt_set_int(m_swr_ctx, "icl", channel_layout, 0);
-		av_opt_set_int(m_swr_ctx, "ich", av_get_channel_layout_nb_channels(channel_layout), 0);
+	void SetInputChannelLayout(AVChannelLayout *channel_layout) {
+		av_opt_set_int(m_swr_ctx, "in_channel_layout", channel_layout->u.mask, 0);
 	}
 
 	void SetInputSampleFormat(AVSampleFormat sample_format) {
-		av_opt_set_int(m_swr_ctx, "isf", sample_format, 0);
+		av_opt_set_sample_fmt(m_swr_ctx, "in_sample_fmt", sample_format, 0);
 	}
 
 	void SetInputSampleRate(int sample_rate) {
-		av_opt_set_int(m_swr_ctx, "isr", sample_rate, 0);
+		av_opt_set_int(m_swr_ctx, "in_sample_rate", sample_rate, 0);
 	}
 
-	void SetOutputChannelLayout(int64_t channel_layout) {
-		av_opt_set_int(m_swr_ctx, "ocl", channel_layout, 0);
-		av_opt_set_int(m_swr_ctx, "och", av_get_channel_layout_nb_channels(channel_layout), 0);
+	void SetOutputChannelLayout(AVChannelLayout *channel_layout) {
+		av_opt_set_int(m_swr_ctx, "out_channel_layout", channel_layout->u.mask, 0);
 	}
 
 	void SetOutputSampleFormat(AVSampleFormat sample_format) {
-		av_opt_set_int(m_swr_ctx, "osf", sample_format, 0);
+		av_opt_set_sample_fmt(m_swr_ctx, "out_sample_fmt", sample_format, 0);
 	}
 
 	void SetOutputSampleRate(int sample_rate) {
-		av_opt_set_int(m_swr_ctx, "osr", sample_rate, 0);
+		av_opt_set_int(m_swr_ctx, "out_sample_rate", sample_rate, 0);
 	}
 
 	int Init() {
