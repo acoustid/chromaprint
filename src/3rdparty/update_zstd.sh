4e3b4838daf572fa73b1eccf41bc30255afe6bc5
Lukáš Lalinský <lalinsky@gmail.com>
2023-03-05 07:41:28 +0100

    .

--- src/3rdparty/update_zstd.sh
+++ src/3rdparty/update_zstd.sh
@@ -2,5 +2,14 @@
 
 set -eux
 
-cd $(dirname $0)/../..
-git subtree pull --squash --prefix src/3rdparty/zstd https://github.com/facebook/zstd v1.5.4
+cd $(dirname $0)
+
+rm -rf zstd-tmp
+git clone -b v1.5.4 https://github.com/facebook/zstd zstd-tmp
+cd zstd-tmp/build/single_file_libs
+./create_single_file_library.sh
+cd ../../..
+mv zstd-tmp/build/single_file_libs/zstd.c zstd/zstd.c
+mv zstd-tmp/lib/zstd.h zstd/zstd.h
+mv zstd-tmp/COPYING zstd/COPYING
+rm -rf zstd-tmp
